c45a9386eb2387b2fda430e5ab36d82ba392cf05
Devanshu Sabnani <devanshusabnani2611@gmail.com>
2023-09-26 18:45:34 -0400

    added advanced authentication features for login

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { auth } from "../../components/firebase";
 import { useRouter } from "expo-router";
 import { validateEmail } from "../../components/auth";
+import React from "react";
 
 export default function Page() {
   const [email, setEmail] = useState("");
@@ -20,61 +21,54 @@ export default function Page() {
   const router = useRouter();
   const [isLoggingIn, setIsLoggingIn] = useState(false);
 
+  const isValidPassword = (pass: string) => {
+    const hasNumber = /\d/;
+    const hasLetter = /[a-zA-Z]/;
+    return pass.length >= 8 && hasNumber.test(pass) && hasLetter.test(pass);
+  };
+
   function handleSignIn() {
     setIsLoggingIn(true);
+
     if (email === "") {
-      toasts.show({
-        title: "Email cannot be empty",
-        bgColor: "red.500",
-        duration: 2000,
-      });
+      toasts.show({ title: "Email cannot be empty", bgColor: "red.500", duration: 2000 });
       setIsLoggingIn(false);
       return;
     }
+
     if (!validateEmail(email)) {
-      toasts.show({
-        title: "Invalid email",
-        bgColor: "red.500",
-        duration: 2000,
-      });
+      toasts.show({ title: "Invalid email", bgColor: "red.500", duration: 2000 });
       setIsLoggingIn(false);
-
       return;
     }
+
     if (password === "") {
+      toasts.show({ title: "Password cannot be empty", bgColor: "red.500", duration: 2000 });
+      setIsLoggingIn(false);
+      return;
+    }
+
+    if (!isValidPassword(password)) {
       toasts.show({
-        title: "Password cannot be empty",
+        title: "Password must be at least 8 characters long and contain both letters and numbers",
         bgColor: "red.500",
         duration: 2000,
       });
       setIsLoggingIn(false);
-
       return;
     }
 
     signInWithEmailAndPassword(auth, email.toLowerCase(), password)
       .then(() => {
-        toasts.show({
-          title: "Sign in successful",
-          bgColor: "green.500",
-          duration: 2000,
-        });
+        toasts.show({ title: "Sign in successful", bgColor: "green.500", duration: 2000 });
         router.replace("/(tabs)/");
-        setIsLoggingIn(false);
       })
       .catch((error) => {
-        const errorMessage = error.message;
-        toasts.show({
-          title: errorMessage,
-          bgColor: "red.500",
-          duration: 2000,
-        });
+        toasts.show({ title: error.message, bgColor: "red.500", duration: 2000 });
         setIsLoggingIn(false);
-
-        return;
       });
-    setIsLoggingIn(false);
   }
+
   return (
     <View>
       <VStack px={"40px"} py={"40px"} alignItems={"center"} space={5}>
@@ -85,6 +79,7 @@ export default function Page() {
           placeholder="email"
           value={email}
           onChangeText={setEmail}
+          autoCapitalize="none"
         />
         <Input
           size="lg"
@@ -92,6 +87,7 @@ export default function Page() {
           type="password"
           value={password}
           onChangeText={setPassword}
+          autoCapitalize="none"
         />
         <Button w="full" onPress={handleSignIn} isDisabled={isLoggingIn}>
           Sign in
