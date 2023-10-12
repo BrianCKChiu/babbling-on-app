f063c902d61d93b17dc6033554ffd88129128a6d
Devanshu Sabnani <devanshusabnani2611@gmail.com>
2023-10-12 14:43:41 -0400

    changed password validation conditions

--- components/auth/validatePassword.ts
+++ components/auth/validatePassword.ts
@@ -3,3 +3,17 @@ export const isValidPassword = (pass: string) => {
   const hasLetter = /[a-zA-Z]/;
   return pass.length >= 8 && hasNumber.test(pass) && hasLetter.test(pass);
 };
+
+// version 2 to add after development:
+// export const isValidPassword = (pass: string) => {
+//   const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/;
+//   return passwordRegex.test(pass);
+// };
+
+
+//Conditions of version 2:
+// At least one number
+// At least one letter (uppercase or lowercase
+// At least one special character
+// No whitespace characters
+// Minimum length of 8 characters
