27b0577f532475572516e1d4224485d4c32a2c38
Brian Chiu <brianchiu33@gmail.com>
2023-11-15 00:32:11 -0500

    configure back button in auth signup

---
+++ styles/backNavButtonStyle.ts
@@ -0,0 +1,11 @@
+export const backNavBtnStyle = {
+  bgColor: "white",
+  borderColor: "gray.100",
+  borderRadius: "full",
+  display: "flex",
+  pl: "4",
+  justifyItems: "center",
+  w: "48px",
+  h: "48px",
+  shadow: "1",
+};
